1a0fdc3c019c8d9ff8c7b95653be6460fd142e48
DESKTOP-RTMDG93\pay_b <sodamlee315@gmail.com>
2022-01-22 19:02:46 +0900

    request module 실습

--- request.js
+++ request.js
@@ -1,4 +1,14 @@
 const request = require('request');
-request('http://naver.com', function (error, response, body) {
-  console.log('body:', body); 
+var parseString = require('xml2js').parseString;
+
+request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function (error, response, body) {
+    var xml = body
+    parseString(xml, function (err, result) {
+        console.dir(result.rss.channel[0].item[0].description[0].header[0].wf[0]);
+        //#work2 wf 태그 안에 있는 기상 예보 값을 불러오기
+    });
+});
+
+request('http://localhost:3000', function (error, response, body) {
+    console.log(body);
 });
