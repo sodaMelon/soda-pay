d538af2f1a11333b4d94310592a490d33262a4b7
DESKTOP-RTMDG93\pay_b <sodamlee315@gmail.com>
2022-01-22 19:25:54 +0900

    불필요한 소스코드 정리

--- expressServer.js
+++ expressServer.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.use(express.static(path.join(__dirname, 'public')));//to use static asset
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
@@ -27,7 +29,7 @@ app.post('/inputTest', function(req, res){
     console.log('request!!');
     console.log(req.body.userIdBody);
     console.log(req.body.userPasswordBody);
-    res.json(1);
+    res.json(1123123);
 })
 
 app.post('/postTest', function(req, res){
