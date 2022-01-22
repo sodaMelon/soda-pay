27e153533b152de6a022acb10806b747f5134897
DESKTOP-RTMDG93\pay_b <sodamlee315@gmail.com>
2022-01-22 19:47:46 +0900

    /authResult 인증결과 전송 완성

--- fintechServer.js
+++ fintechServer.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const request = require('request');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -16,6 +17,30 @@ app.get('/design', function(req, res){
 
 app.get('/signup', function(req, res){
     res.render('signup')
+})
+
+app.get('/authResult', function(req, res){
+    console.log('authResult');
+    console.log(req.query);
+    var authCode = req.query.code;
+    var option = {
+        method : "POST",
+        url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+        header : {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        form : {
+            code : authCode,
+            client_id : 'q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF',
+            client_secret : 'yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy',
+            redirect_uri : 'http://localhost:3000/authResult',
+            grant_type : 'authorization_code'
+        }
+    }
+    request(option, function (error, response, body) {
+        console.log(body);
+        res.json(body);
+    });
 })
 
 app.listen(3000)
